f85be78f40aa4df4f4e6e07b7f397c467c3629b8
Ignacio Tampe <Igtampe@gmail.com>
2021-08-25 20:13:04 -0400

    LandView Plotter (Not finished)

---
+++ LandViewPlotter/CountryPlotter.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Igtampe.LandViewPlotter {
+    partial class CountryPlotter {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.MainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // MainTableLayoutPanel
+            // 
+            this.MainTableLayoutPanel.ColumnCount = 1;
+            this.MainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.MainTableLayoutPanel.Location = new System.Drawing.Point(12, 12);
+            this.MainTableLayoutPanel.Name = "MainTableLayoutPanel";
+            this.MainTableLayoutPanel.RowCount = 3;
+            this.MainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.MainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.MainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.MainTableLayoutPanel.Size = new System.Drawing.Size(577, 396);
+            this.MainTableLayoutPanel.TabIndex = 0;
+            // 
+            // CountryPlotter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 585);
+            this.Controls.Add(this.MainTableLayoutPanel);
+            this.Name = "CountryPlotter";
+            this.Text = "CountryPlotter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel MainTableLayoutPanel;
+    }
+}
---
+++ LandViewPlotter/LandViewGraphicsEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Igtampe.Neco.Common;
+
+namespace Igtampe.LandViewPlotter {
+    public static class LandViewGraphicsEngine {
+
+        
+
+    }
+}
