96c81f74385c365addb52b818ba862c0a05412c8
Ignacio Tampe <Igtampe@gmail.com>
2021-09-13 20:39:02 -0400

    Move PasswordChangeRequest to new Requests Namespace

--- Neco/Requests/PasswordChangeRequest.cs
+++ Neco/Requests/PasswordChangeRequest.cs
@@ -1,10 +1,10 @@
-﻿namespace Igtampe.Neco.Common.Auth {
+﻿namespace Igtampe.Neco.Common.Requests {
 
     /// <summary>Request to change a password</summary>
     public class PasswordChangeRequest {
 
-        /// <summary>ID of the user to change the password of</summary>
-        public string UserID { get; set; }
+        /// <summary>ID of the session this PCR comes from</summary>
+        public System.Guid SessionID {get; set;}
 
         /// <summary>User's current password</summary>
         public string CurrentPassword {get; set;}
@@ -17,20 +17,20 @@
         /// <returns>True if and only if the object is a Password Change Request, and all fields match</returns>
         public override bool Equals(object obj) {
             if (obj is PasswordChangeRequest C) {
-                return C.UserID == UserID &&
+                return C.SessionID == SessionID &&
                     C.CurrentPassword == CurrentPassword &&
                     C.NewPassword == NewPassword;
             }
             return false;
         }
 
-        /// <summary>Gets a hash code for this TaxBracket. Delegates to <see cref="UserID"/></summary>
+        /// <summary>Gets a hash code for this TaxBracket. Delegates to <see cref="SessionID"/></summary>
         /// <returns></returns>
-        public override int GetHashCode() { return UserID.GetHashCode(); }
+        public override int GetHashCode() { return SessionID.GetHashCode(); }
 
         /// <summary>Creates a string representation of this TaxBracket</summary>
         /// <returns>"ID} : {Name}, From {Start} to {End} at {Rate}%</returns>
-        public override string ToString() { return $"{UserID} Password Change Request"; }
+        public override string ToString() { return $"{SessionID} Password Change Request"; }
 
 
     }
