f0560c686c0d4f6fa84b8cd25d2876b61c971c50
Ignacio Tampe <Igtampe@gmail.com>
2021-09-17 13:20:20 -0400

    UMSAT Controller Rework

--- Neco.Backend/Controllers/UMSATController.cs
+++ Neco.Backend/Controllers/UMSATController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Igtampe.Neco.Common.UMSAT;
+using Igtampe.Neco.Common.UMSAT.Requests;
+using Igtampe.Neco.Common;
 using Igtampe.Neco.Data;
 
 namespace Igtampe.Neco.Backend.Controllers {
@@ -14,15 +16,15 @@ namespace Igtampe.Neco.Backend.Controllers {
     [ApiController]
     public class UMSATController: Controller {
 
-        private readonly NecoContext _context;
-        public UMSATController(NecoContext context) {_context = context;}
+        private readonly NecoContext NecoDB;
+        public UMSATController(NecoContext context) {NecoDB = context;}
 
         // GET: UMSAT
         [HttpGet]
         public async Task<IActionResult> Index(int? start, int? end) {
             int realstart = start!=null ? (int)start : 0;
             int realend = end != null ? (int)end : 20;
-            return Ok(await _context.Asset
+            return Ok(await NecoDB.Asset
                 .Include(a => a.IncomeItem).ThenInclude(m => m.LocalJurisdiction)
                 .Include(a => a.IncomeItem).ThenInclude(m => m.FederalJurisdiction)
                 .Include(a => a.IncomeItem).ThenInclude(m => m.Apartments)
@@ -37,7 +39,7 @@ namespace Igtampe.Neco.Backend.Controllers {
         public async Task<IActionResult> Details(Guid? id) {
             if (id == null) { return NotFound(); }
 
-            var asset = await _context.Asset.Include(a => a.IncomeItem).ThenInclude(m => m.LocalJurisdiction)
+            var asset = await NecoDB.Asset.Include(a => a.IncomeItem).ThenInclude(m => m.LocalJurisdiction)
                 .Include(a => a.IncomeItem).ThenInclude(m => m.FederalJurisdiction)
                 .Include(a => a.IncomeItem).ThenInclude(m => m.Apartments)
                 .Include(a => a.IncomeItem).ThenInclude(m => m.Businesses)
@@ -50,39 +52,107 @@ namespace Igtampe.Neco.Backend.Controllers {
             return Ok(asset);
         }
 
-        // POST: UMSAT
+        //POST: UMSAT
         [HttpPost]
-        public async Task<IActionResult> Create(Asset asset) {
-            if (asset.ID != Guid.Empty) { BadRequest("Asset has an ID. Did you mean to edit it?"); }
-            _context.Add(asset);
-            await _context.SaveChangesAsync();
-            return Created($"UMSAT/{asset.ID}", asset);
+        public async Task<IActionResult> CreateEdit(Guid SessionID) {
+            Session S = SessionManager.Manager.FindSession(SessionID);
+            if (S == null) { return Unauthorized("Invalid session"); }
+
+            var asset = await NecoDB.Asset.Include(a => a.IncomeItem).ThenInclude(m => m.LocalJurisdiction)
+                .Include(a => a.IncomeItem).ThenInclude(m => m.FederalJurisdiction)
+                .Include(a => a.IncomeItem).ThenInclude(m => m.Apartments)
+                .Include(a => a.IncomeItem).ThenInclude(m => m.Businesses)
+                .Include(a => a.IncomeItem).ThenInclude(m => m.Hotels)
+                .Include(a => a.Plot).ThenInclude(m => m.District).ThenInclude(m => m.Country)
+                .Include(a => a.Owner).ThenInclude(m => m.Type).Where(m => m.Owner.ID==S.UserID)
+                .OrderByDescending(m=>m.UpdateDate).ToListAsync();
+            
+            return Ok(asset);
         }
 
-        // PUT: UMSAT/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Edit(Guid id, Asset asset) {
-            if (id != asset.ID) { return NotFound(); }
+        // POST: UMSAT/Mod
+        [HttpPost("Mod")]
+        public async Task<IActionResult> CreateEdit(AssetModRequest Request) {
+            Session S = SessionManager.Manager.FindSession(Request.SessionID);
+            if (S == null) { return Unauthorized("Invalid session"); }
 
-            try {
-                _context.Update(asset);
-                await _context.SaveChangesAsync();
-            } catch (DbUpdateConcurrencyException) {
-                if (!AssetExists(asset.ID)) { return NotFound(); } else { throw; }
+            User U = await NecoDB.User.FirstOrDefaultAsync(U => U.ID == S.UserID);
+
+            Asset A;
+
+            if (Request.AssetID == Guid.Empty) {
+                //This is a New Asset
+                A = new() {
+                    Complete = Request.Complete,
+                    CreationDate = DateTime.Now,
+                    Description = Request.Description,
+                    Image = Request.Image,
+                    Name = Request.Name,
+                    Owner = U,
+                    SpecificLocaiton=Request.SpecificLocation,
+                    UpdateDate=DateTime.Now
+                };
+
+                NecoDB.Add(A);
+
+            } else {
+
+                //This is a modified asset.
+                A = await NecoDB.Asset.Include(A=>A.Owner).FirstOrDefaultAsync(A => A.ID == Request.AssetID);
+                if (A == null) { return NotFound("Asset was not found"); }
+                if (A.Owner.ID != S.UserID) { return Unauthorized("Session owner isn't this asset's owner"); }
+                
+                //Update the asset details
+                A.Complete = Request.Complete;
+                A.Description = Request.Description;
+                A.Image = Request.Image;
+                A.Name = Request.Name;
+                A.SpecificLocaiton = Request.SpecificLocation;
+                A.UpdateDate = DateTime.Now;
+
+                NecoDB.Update(A);
             }
 
-            return Ok(asset);
+            //Now save the asset
+            await NecoDB.SaveChangesAsync();
+            return Ok(A);
         }
 
-        //DELETE: UMSAT/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteConfirmed(Guid id) {
-            var asset = await _context.Asset.FindAsync(id);
-            _context.Asset.Remove(asset);
-            await _context.SaveChangesAsync();
-            return Ok(asset);
+        // POST: UMSAT/Transfer
+        [HttpPost("Transfer")]
+        public async Task<IActionResult> Transfer(AssetTransferRequest Request) {
+            Session S = SessionManager.Manager.FindSession(Request.SessionID);
+            if (S == null) { return Unauthorized("Invalid session"); }
+
+            Asset A = await NecoDB.Asset.Include(A => A.Owner).FirstOrDefaultAsync(A => A.ID == Request.AssetID);
+            if (A == null) { return NotFound("Asset was not found"); }
+            if (A.Owner.ID != S.UserID) { return Unauthorized("Session owner isn't this asset's owner"); }
+
+            User U = await NecoDB.User.FirstOrDefaultAsync(U => U.ID == Request.NewOwnerID);
+            if (U == null) { return NotFound("New owner was not found"); }
+
+            A.Owner = U;
+            NecoDB.Update(A);
+            await NecoDB.SaveChangesAsync();
+
+            return Ok(A);
         }
 
-        private bool AssetExists(Guid id) { return _context.Asset.Any(e => e.ID == id); }
+        //POST: UMSAT/Del
+        [HttpPost("Del")]
+        public async Task<IActionResult> Delete(AssetDeleteRequest Request) {
+            Session S = SessionManager.Manager.FindSession(Request.SessionID);
+            if (S == null) { return Unauthorized("Invalid session"); }
+
+            Asset A = await NecoDB.Asset.Include(A => A.Owner).FirstOrDefaultAsync(A => A.ID == Request.AssetID);
+            if (A == null) { return NotFound("Asset was not found"); }
+            if (A.Owner.ID != S.UserID) { return Unauthorized("Session owner isn't this asset's owner"); }
+
+            NecoDB.Remove(A);
+            await NecoDB.SaveChangesAsync();
+            return Ok(A);
+
+            
+        }
     }
 }
