a5f20516610454d21314b47e793562fae3bfa041
Ignacio Tampe <Igtampe@gmail.com>
2021-09-18 14:51:25 -0400

    Lets see if this cosmos thing works

---
+++ Neco.Data/NecoCosmosContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igtampe.Neco.Data {
+
+    /// <summary>Context for using Neco on Cosmos rather than on SQL Server</summary>
+    public class NecoCosmosContext:NecoContext {
+
+        /// <summary>Overrides onConfiguring to use <see cref="Constants.ConnectionString"/></summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            string[] CosmosString = File.ReadAllLines("Cosmos.txt");
+            if (CosmosString.Length < 1) { throw new InvalidOperationException("I need one of these lines at least"); }
+            optionsBuilder.UseCosmos(CosmosString[0],"neco"); 
+        }
+
+    }
+}
