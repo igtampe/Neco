0654c9a717a3fba8486c61f52714798b43b4fabd
Ignacio Tampe <Igtampe@gmail.com>
2021-09-22 17:13:16 -0400

    Migrate Road Width to DB

---
+++ Neco.Data/Migrations/20210922211202_AddWidth.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Igtampe.Neco.Data.Migrations
+{
+    public partial class AddWidth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Width",
+                table: "Road",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "Road");
+        }
+    }
+}
