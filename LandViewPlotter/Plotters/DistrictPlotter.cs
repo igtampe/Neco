f0deb398789c4075266c325a1c9d2f9cc9de0ebf
Ignacio Tampe <Igtampe@gmail.com>
2021-09-22 23:54:02 -0400

    District CRU (no D). We're not going to let people delete Plots or Districts anymore. Maybe Roads

--- LandViewPlotter/Plotters/DistrictPlotter.cs
+++ LandViewPlotter/Plotters/DistrictPlotter.cs
@@ -13,9 +13,9 @@ using Igtampe.Neco.Data;
 namespace Igtampe.LandViewPlotter {
     public partial class DistrictPlotter: Form {
 
-        private NecoContext NecoDB;
-        private District MyDistrict;
-        private bool Edited;
+        private readonly NecoContext NecoDB;
+        public District MyDistrict { get; private set; }
+        public bool Edited { get; private set; }
 
         public DistrictPlotter(District D, NecoContext NecoDB) {
             InitializeComponent();
@@ -35,7 +35,6 @@ namespace Igtampe.LandViewPlotter {
             PlotContextMenu.Opening += PlotContextMenu_Opening;
             NewPlotToolStripMenuItem.Click += NewPlotToolStripMenuItem_Click;
             EditPlotToolStripMenuItem.Click += EditPlotToolStripMenuItem_Click;
-            DeletePlotToolStripMenuItem.Click += DeletePlotToolStripMenuItem_Click;
             #endregion
 
             PopulateData();
@@ -59,12 +58,38 @@ namespace Igtampe.LandViewPlotter {
         }
 
         private void NewPlotToolStripMenuItem_Click(object sender, EventArgs e) {
+
+            Plot P = new() {
+                District = MyDistrict,
+                Status = PlotStatus.NOT_FOR_SALE,
+            };
+
+            PlotPlotter PPlotter = new(P, NecoDB);
+            if (PPlotter.ShowDialog()==DialogResult.OK) {
+                //Add the plot
+                MyDistrict.Plots.Add(P);
+                PopulateData();
+            }
         }
 
         private void EditPlotToolStripMenuItem_Click(object sender, EventArgs e) {
+
+            int Index = GetSelectedPlotIndex();
+            if (Index == -1) { return; }
+
+            Plot P = MyDistrict.Plots[Index];
+            PlotPlotter PPlotter = new(P, NecoDB);
+            if (PPlotter.ShowDialog() == DialogResult.OK) {
+                if (!PPlotter.Edited) { return; }
+                MyDistrict.Plots[Index] = PPlotter.MyPlot;
+                PopulateData();
+            }
+
         }
 
-        private void DeletePlotToolStripMenuItem_Click(object sender, EventArgs e) {
+        private int GetSelectedPlotIndex() {
+            if (PlotsListView.SelectedIndices.Count == 0) { return -1; } 
+            else { return PlotsListView.SelectedIndices[0]; }
         }
 
         private void PPSMBox_GotFocus(object sender, EventArgs e) { PPSMLabel.Text = "Price per m²*"; }
@@ -114,7 +139,7 @@ namespace Igtampe.LandViewPlotter {
         }
 
         private void PointsBox_TextChanged(object sender, EventArgs e) {
-            if (!ValidatePoints(PointsBox.Text)) { return; }
+            if (!LandViewUtils.ValidatePoints(PointsBox.Text,3)) { return; }
             MyDistrict.Points = PointsBox.Text;
             MarkEdited();
         }
@@ -122,29 +147,13 @@ namespace Igtampe.LandViewPlotter {
         private void PointsBox_LostFocus(object sender, EventArgs e) {
             PointsLabel.Text = "Points";
             if (MyDistrict.Points == PointsBox.Text) { return; }
-            if (!ValidatePoints(PointsBox.Text)) {
+            if (!LandViewUtils.ValidatePoints(PointsBox.Text,3)) {
                 ShowCriticalMessagebox("Could not parse points! Please fix them before continuing");
                 PointsBox.Focus();
                 return;
             }
             MyDistrict.Points = PointsBox.Text;
             MarkEdited();
-        }
-
-        private bool ValidatePoints(string Points) {
-
-            //Ensure blank ones can be actually saved.
-            if (Points.Length == 0) { return true; }
-
-            //First let's split this by semicolon
-            foreach (string ppair in Points.Split(";")) {
-                //Split it again by colon
-                string[] ppairsplit = ppair.Split(",");
-                if (ppairsplit.Count() != 2) { return false; } //If we have more than three coords then no
-                if (!int.TryParse(ppairsplit[0], out int _)) { return false; } //Test to ensure both can be
-                if (!int.TryParse(ppairsplit[1], out int _)) { return false; } //parsed as ints
-            }
-            return true;
         }
 
         private void NameBox_GotFocus(object sender, EventArgs e) {
