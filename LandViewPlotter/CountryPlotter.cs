f2549a53bd0392066476acab3c0576f02bcce761
Ignacio Tampe <Igtampe@gmail.com>
2021-09-22 22:12:06 -0400

    Country Plotter Work (Missing CRUD Operations for subitems)

--- LandViewPlotter/CountryPlotter.cs
+++ LandViewPlotter/CountryPlotter.cs
@@ -7,11 +7,267 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Igtampe.Neco.Common.LandView;
+using Igtampe.Neco.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Igtampe.LandViewPlotter {
     public partial class CountryPlotter: Form {
+
+        private NecoContext NecoDB;
+        private Country MyCountry;
+        private bool Edited;
+
         public CountryPlotter() {
             InitializeComponent();
+
+            //Mira if the designer won't respect me then:
+            #region Event handlers
+            Shown += OpenMenuItem_Click;
+            FormClosing += CountryPlotter_FormClosing;
+            NameBox.GotFocus += NameBox_GotFocus;
+            NameBox.LostFocus += NameBox_LostFocus;
+            WidthBox.GotFocus += WidthBox_GotFocus;
+            WidthBox.LostFocus += WidthBox_LostFocus;
+            HeightBox.GotFocus += HeightBox_GotFocus;
+            HeightBox.LostFocus += HeightBox_LostFocus;
+            BankAccountBox.GotFocus += BankAccountBox_GotFocus;
+            BankAccountBox.LostFocus += BankAccountBox_LostFocus;
+            PPSMBox.GotFocus += PPSMBox_GotFocus;
+            PPSMBox.LostFocus += PPSMBox_LostFocus;
+            DistrictsContextMenu.Opening += DistrictsContextMenu_Opening;
+            RoadContextMenu.Opening += RoadContextMenu_Opening;
+            newDistrictToolStripMenuItem.Click += NewDistrictToolStripMenuItem_Click;
+            EditDistrictToolStripMenuItem.Click += EditDistrictToolStripMenuItem_Click;
+            DeleteDistrictToolStripMenuItem.Click += DeleteDistrictToolStripMenuItem_Click;
+            newRoadToolStripMenuItem.Click += NewRoadToolStripMenuItem_Click;
+            EditRoadToolStripMenuItem.Click += EditRoadToolStripMenuItem_Click;
+            DeleteRoadToolStripMenuItem.Click += DeleteRoadToolStripMenuItem_Click;
+            CountryPickerToolStripMenuItem.Click += OpenMenuItem_Click;
+            saveToolStripMenuItem.Click += SaveToolStripMenuItem_Click;
+            exitToolStripMenuItem.Click += ExitToolStripMenuItem_Click;
+            #endregion
+
         }
+
+        /// <summary>Populates the form with information</summary>
+        private void PopulateData() {
+            NameBox.Text = MyCountry.Name;
+            WidthBox.Text = MyCountry.Width+"";
+            HeightBox.Text = MyCountry.Height+"";
+            BankAccountBox.Text = MyCountry.FederalBankAccount?.ID;
+            PPSMBox.Text = MyCountry.PricePerSquareMeter + "";
+            PreviewPictureBox.Image = LandViewGraphicsEngine.GenerateCountryImage(MyCountry);
+
+            DistrictsListView.Items.Clear();
+            foreach (District D in MyCountry.Districts) {DistrictsListView.Items.Add(D.Name);}
+
+            RoadsListView.Items.Clear();
+            foreach (Road R in MyCountry.Roads) { RoadsListView.Items.Add(R.Name); }
+        }
+
+        private void CountryPlotter_FormClosing(object sender, FormClosingEventArgs e) {
+            if (Edited) {
+                switch (MessageBox.Show("There are unsaved changes! Would you like to save?", "Are You Sure?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question)) {
+                    case DialogResult.Yes:
+                        SaveToolStripMenuItem_Click(sender, null);
+                        break;
+                    case DialogResult.No:
+                        return;
+                    case DialogResult.None:
+                    case DialogResult.Cancel:
+                    case DialogResult.OK:
+                    case DialogResult.Abort:
+                    case DialogResult.Retry:
+                    case DialogResult.Ignore:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+        }
+
+
+        #region MainContextMenu
+
+        private void PreviewPictureBox_Click(object sender, EventArgs e) {
+            if (PreviewPictureBox.Image == null) { return; }
+            new PreviewForm(PreviewPictureBox.Image).ShowDialog();
+        }
+
+        private void OpenMenuItem_Click(object sender, EventArgs e) {
+            //Load all countries
+            if (NecoDB != null) {
+
+                FormClosingEventArgs E = new(CloseReason.None,false);
+                CountryPlotter_FormClosing(sender, E);
+                if (E.Cancel) { return; }
+                
+                NecoDB.Dispose(); 
+            
+            }
+            NecoDB = new();
+
+            List<Country> Countries = NecoDB.Country
+                    .Include(C => C.Districts).ThenInclude(D => D.Plots).ThenInclude(P => P.Owner).ThenInclude(U => U.Type)
+                    .Include(C => C.Districts).ThenInclude(D => D.DistrictBankAccount)
+                    .Include(C => C.FederalBankAccount)
+                    .ToList();
+
+            CountryPicker CP = new(Countries);
+            if (CP.ShowDialog() != DialogResult.OK && MyCountry == null) { Close(); return; }
+
+            MyCountry = CP.SelectedCountry;
+            PopulateData();
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (MyCountry.ID == Guid.Empty) { NecoDB.Add(MyCountry); } else { NecoDB.Update(MyCountry); }
+            NecoDB.SaveChanges();
+            Edited = false;
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e) { Close(); }
+
+        #endregion
+
+        #region DistrictsContextMenu
+
+        private void DistrictsContextMenu_Opening(object sender, System.ComponentModel.CancelEventArgs e) {
+            bool EnableEditOptions = DistrictsListView.SelectedIndices.Count > 0;
+            EditDistrictToolStripMenuItem.Enabled = EnableEditOptions;
+            DeleteDistrictToolStripMenuItem.Enabled = EnableEditOptions;
+        }
+
+        private void NewDistrictToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void EditDistrictToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void DeleteDistrictToolStripMenuItem_Click(object sender, EventArgs e) {
+            //Find the district
+            
+    
+        }
+
+        #endregion
+
+        #region RoadContextMenu
+
+        private void RoadContextMenu_Opening(object sender, System.ComponentModel.CancelEventArgs e) {
+            bool EnableEditOptions = RoadsListView.SelectedIndices.Count > 0;
+            EditRoadToolStripMenuItem.Enabled = EnableEditOptions;
+            DeleteRoadToolStripMenuItem.Enabled = EnableEditOptions;
+        }
+
+        private void NewRoadToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void EditRoadToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void DeleteRoadToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        #endregion
+
+        #region TextBox Handlers
+
+        private void BankAccountBox_GotFocus(object sender, System.EventArgs e) {BankAccountLabel.Text = "Bank Account*";}
+
+        private void BankAccountBox_LostFocus(object sender, System.EventArgs e) {
+            BankAccountLabel.Text = "Bank Account";
+            if (MyCountry.FederalBankAccount?.ID == BankAccountBox.Text) { return; }
+            if (string.IsNullOrWhiteSpace(BankAccountBox.Text)) {
+                MyCountry.FederalBankAccount = null;
+                MarkEdited();
+                return;
+            }
+
+            var B = NecoDB.BankAccount.Find(BankAccountBox.Text);
+            if (B==null) {
+                ShowCriticalMessagebox($"Could not find bank account with ID '{BankAccountBox.Text}'");
+                BankAccountBox.Text = MyCountry.FederalBankAccount?.ID;
+                return; 
+            }
+            
+            if (B.Closed) {
+                ShowCriticalMessagebox($"Bank Account with ID '{BankAccountBox.Text}' is closed");
+                BankAccountBox.Text = MyCountry.FederalBankAccount?.ID;
+                return;
+            }
+
+            MyCountry.FederalBankAccount = B;
+            MarkEdited();
+
+        }
+
+        private void HeightBox_GotFocus(object sender, System.EventArgs e) {HeightLabel.Text = "Height*";}
+
+        private void HeightBox_LostFocus(object sender, System.EventArgs e) {
+            HeightLabel.Text = "Height";
+            if (!int.TryParse(HeightBox.Text, out int H)) {
+                ShowCriticalMessagebox($"Could not parse '{HeightBox.Text}' as an integer");
+                HeightBox.Text = MyCountry.Width + "";
+            } else {
+                if (H == MyCountry.Height) { return; }
+                MyCountry.Height = H;
+                MarkEdited();
+            }
+        }
+
+        private void WidthBox_GotFocus(object sender, System.EventArgs e) {WidthLabel.Text = "Width*";}
+
+        private void WidthBox_LostFocus(object sender, System.EventArgs e) {
+            WidthLabel.Text = "Width";
+            if (!int.TryParse(WidthBox.Text, out int W)) {
+                ShowCriticalMessagebox($"Could not parse '{WidthBox.Text}' as an integer");
+                WidthBox.Text = MyCountry.Width + "";
+            } else {
+                if (W == MyCountry.Width) { return; }
+                MyCountry.Width = W;
+                MarkEdited();
+            }
+        }
+
+        private void NameBox_GotFocus(object sender, System.EventArgs e) {NameLabel.Text = "Name*";}
+
+        private void NameBox_LostFocus(object sender, System.EventArgs e) {
+            NameLabel.Text = "Name";
+            if (MyCountry.Name == NameBox.Text) { return; }
+            MyCountry.Name = NameBox.Text;
+            MarkEdited();
+        }
+
+        private void PPSMBox_GotFocus(object sender, EventArgs e) {
+            PPSMLabel.Text = "Price per m²*";
+        }
+
+        private void PPSMBox_LostFocus(object sender, EventArgs e) {
+            PPSMLabel.Text = "Price per m²";
+            if (!int.TryParse(PPSMBox.Text, out int P)) {
+                ShowCriticalMessagebox($"Could not parse '{PPSMBox.Text}' as an integer");
+                PPSMBox.Text = MyCountry.PricePerSquareMeter + "";
+            } else {
+                if (P == MyCountry.PricePerSquareMeter) { return; }
+                MyCountry.PricePerSquareMeter = P;
+                MarkEdited();
+            }
+        }
+
+        #endregion
+
+        private void MarkEdited() {
+            Edited = true;
+            PreviewPictureBox.Image = LandViewGraphicsEngine.GenerateCountryImage(MyCountry);
+        }
+
+        private static void ShowCriticalMessagebox(string message) {MessageBox.Show(message, "No", MessageBoxButtons.OK, MessageBoxIcon.Error);}
+
     }
 }
