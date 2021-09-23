f9bd77d5204681527a3e37f22deaa5e6990c73a3
Ignacio Tampe <Igtampe@gmail.com>
2021-09-23 01:35:50 -0400

    About Form (Basic but it there)

--- LandViewPlotter/AboutForm.Designer.cs
+++ LandViewPlotter/AboutForm.Designer.cs
@@ -24,15 +24,76 @@ namespace Igtampe.LandViewPlotter {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.MainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.LogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.LandViewLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.OkButton = new System.Windows.Forms.Button();
+            this.MainTableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MainTableLayoutPanel
+            // 
+            this.MainTableLayoutPanel.ColumnCount = 1;
+            this.MainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.MainTableLayoutPanel.Controls.Add(this.LogoPictureBox, 0, 0);
+            this.MainTableLayoutPanel.Controls.Add(this.LandViewLabel, 0, 1);
+            this.MainTableLayoutPanel.Controls.Add(this.label2, 0, 2);
+            this.MainTableLayoutPanel.Controls.Add(this.OkButton, 0, 3);
+            this.MainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainTableLayoutPanel.Name = "MainTableLayoutPanel";
+            this.MainTableLayoutPanel.RowCount = 4;
+            this.MainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.MainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.MainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.MainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.MainTableLayoutPanel.Size = new System.Drawing.Size(310, 296);
+            this.MainTableLayoutPanel.TabIndex = 0;
+            // 
+            // LogoPictureBox
+            // 
+            this.LogoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogoPictureBox.Image = global::Igtampe.LandViewPlotter.Properties.Resources.Main;
+            this.LogoPictureBox.Location = new System.Drawing.Point(10, 10);
+            this.LogoPictureBox.Margin = new System.Windows.Forms.Padding(10);
+            this.LogoPictureBox.Name = "LogoPictureBox";
+            this.LogoPictureBox.Size = new System.Drawing.Size(290, 182);
+            this.LogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LogoPictureBox.TabIndex = 2;
+            this.LogoPictureBox.TabStop = false;
+            // 
+            // LandViewLabel
+            // 
+            this.LandViewLabel.AutoSize = true;
+            this.LandViewLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LandViewLabel.Font = new System.Drawing.Font("Arial", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.LandViewLabel.Location = new System.Drawing.Point(3, 202);
+            this.LandViewLabel.Name = "LandViewLabel";
+            this.LandViewLabel.Size = new System.Drawing.Size(304, 37);
+            this.LandViewLabel.TabIndex = 3;
+            this.LandViewLabel.Text = "LandView Version 2.0";
+            this.LandViewLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "(C)2021 Igtampe, No Rights Reserved";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // OkButton
             // 
-            this.OkButton.Location = new System.Drawing.Point(504, 305);
+            this.OkButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.OkButton.Location = new System.Drawing.Point(117, 270);
             this.OkButton.Name = "OkButton";
             this.OkButton.Size = new System.Drawing.Size(75, 23);
-            this.OkButton.TabIndex = 0;
+            this.OkButton.TabIndex = 1;
             this.OkButton.Text = "OK";
             this.OkButton.UseVisualStyleBackColor = true;
             this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
@@ -41,17 +102,25 @@ namespace Igtampe.LandViewPlotter {
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(591, 340);
-            this.Controls.Add(this.OkButton);
+            this.ClientSize = new System.Drawing.Size(310, 296);
+            this.Controls.Add(this.MainTableLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "AboutForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About";
+            this.MainTableLayoutPanel.ResumeLayout(false);
+            this.MainTableLayoutPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TableLayoutPanel MainTableLayoutPanel;
+        private System.Windows.Forms.PictureBox LogoPictureBox;
+        private System.Windows.Forms.Label LandViewLabel;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button OkButton;
     }
 }
