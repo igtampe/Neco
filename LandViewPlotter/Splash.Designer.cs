34f386055e632e158031e9728ea3c4bf2f25f458
Ignacio Tampe <Igtampe@gmail.com>
2021-09-23 14:44:22 -0400

    Remove icon let's not do this on an individual level

--- LandViewPlotter/Splash.Designer.cs
+++ LandViewPlotter/Splash.Designer.cs
@@ -37,7 +37,6 @@ namespace Igtampe.LandViewPlotter {
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(707, 309);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Splash";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LandViewPlotter Splash";
