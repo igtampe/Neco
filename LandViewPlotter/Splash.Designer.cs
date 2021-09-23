097c6590dd7d9ed33479d31fcae7d5334729a91a
Ignacio Tampe <Igtampe@gmail.com>
2021-09-23 14:39:19 -0400

    Add Splash Icon

--- LandViewPlotter/Splash.Designer.cs
+++ LandViewPlotter/Splash.Designer.cs
@@ -24,6 +24,7 @@ namespace Igtampe.LandViewPlotter {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
             this.WaitAndLaunchBWorker = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
@@ -36,6 +37,7 @@ namespace Igtampe.LandViewPlotter {
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(707, 309);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Splash";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LandViewPlotter Splash";
