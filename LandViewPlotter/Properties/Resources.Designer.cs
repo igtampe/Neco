acb4e34d5dd0c4a86927e530c8f3516fb560728b
Ignacio Tampe <Igtampe@gmail.com>
2021-09-23 14:10:35 -0400

    Add Neco Load image

--- LandViewPlotter/Properties/Resources.Designer.cs
+++ LandViewPlotter/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Igtampe.LandViewPlotter.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Load {
+            get {
+                object obj = ResourceManager.GetObject("Load", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Main {
             get {
                 object obj = ResourceManager.GetObject("Main", resourceCulture);
