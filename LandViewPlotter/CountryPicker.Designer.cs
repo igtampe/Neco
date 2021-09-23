de2a144d2aa82ac884235e98f83a65727b9aa97d
Ignacio Tampe <Igtampe@gmail.com>
2021-09-22 22:11:28 -0400

    CountryPicker to execute first

---
+++ LandViewPlotter/CountryPicker.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Igtampe.LandViewPlotter {
+    partial class CountryPicker {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CountriesComboBox = new System.Windows.Forms.ComboBox();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(470, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Country from your Local Neco Database to edit, or select New to add a ne" +
+    "w one";
+            // 
+            // CountriesComboBox
+            // 
+            this.CountriesComboBox.FormattingEnabled = true;
+            this.CountriesComboBox.Location = new System.Drawing.Point(12, 27);
+            this.CountriesComboBox.Name = "CountriesComboBox";
+            this.CountriesComboBox.Size = new System.Drawing.Size(665, 23);
+            this.CountriesComboBox.TabIndex = 1;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(602, 62);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 2;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKClick);
+            // 
+            // CountryPicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 97);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.CountriesComboBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "CountryPicker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pick a Country";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CountriesComboBox;
+        private System.Windows.Forms.Button OKButton;
+    }
+}
