163d566e3ecab1519d66bde1d13673f14e98cbf9
Ignacio Tampe <Igtampe@gmail.com>
2021-09-22 22:10:30 -0400

    Import PreviewForm from UTrace

---
+++ LandViewPlotter/PreviewForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Igtampe.LandViewPlotter {
+    partial class PreviewForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ImageBox = new System.Windows.Forms.PictureBox();
+            this.PictureMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExportFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).BeginInit();
+            this.PictureMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.ImageBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(932, 690);
+            this.panel1.TabIndex = 1;
+            // 
+            // ImageBox
+            // 
+            this.ImageBox.ContextMenuStrip = this.PictureMenuStrip;
+            this.ImageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ImageBox.Location = new System.Drawing.Point(0, 0);
+            this.ImageBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ImageBox.Name = "ImageBox";
+            this.ImageBox.Size = new System.Drawing.Size(932, 690);
+            this.ImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ImageBox.TabIndex = 0;
+            this.ImageBox.TabStop = false;
+            this.ImageBox.Click += new System.EventHandler(this.ImageBox_Click);
+            // 
+            // PictureMenuStrip
+            // 
+            this.PictureMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.PictureMenuStrip.Name = "PictureMenuStrip";
+            this.PictureMenuStrip.Size = new System.Drawing.Size(170, 48);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.copyToolStripMenuItem.Text = "Copy to clipboard";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.CopyToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.saveToolStripMenuItem.Text = "Save to disk";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // ExportFileDialog
+            // 
+            this.ExportFileDialog.DefaultExt = "png";
+            this.ExportFileDialog.FileName = "UserTrace.png";
+            this.ExportFileDialog.Filter = "PNG Files|*.png";
+            this.ExportFileDialog.Title = "Export";
+            // 
+            // PreviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(932, 690);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "PreviewForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Preview";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).EndInit();
+            this.PictureMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImageBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip PictureMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog ExportFileDialog;
+    }
+}
---
+++ LandViewPlotter/PreviewForm.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Igtampe.LandViewPlotter {
+
+    /// <summary>Previews an image</summary>
+    public partial class PreviewForm:Form {
+
+        /// <summary>Creates a preview form to show an image (but without an image)</summary>
+        public PreviewForm() { InitializeComponent(); }
+
+        /// <summary>Creates a preview form to show an image (with an image)</summary>
+        /// <param name="I"></param>
+        public PreviewForm(Image I) : this() { SetImage(I); }
+
+        /// <summary>Sets the image in this previewform</summary>
+        /// <param name="I"></param>
+        public void SetImage(Image I) {
+            ImageBox.Image = I;
+            Size = new Size(I.Width,I.Height);
+        }
+
+        /// <summary>Copies the image to the clipboard</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CopyToolStripMenuItem_Click(object sender,System.EventArgs e) { Clipboard.SetImage(ImageBox.Image); }
+
+        /// <summary>Saves Image to Disk</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveToolStripMenuItem_Click(object sender,System.EventArgs e) {
+            if(ExportFileDialog.ShowDialog() == DialogResult.OK) { ImageBox.Image.Save(ExportFileDialog.FileName,System.Drawing.Imaging.ImageFormat.Png); }
+        }
+
+        /// <summary>Close the preview picturebox if the image is clicked</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ImageBox_Click(object sender,System.EventArgs e) { Close(); }
+    }
+}
