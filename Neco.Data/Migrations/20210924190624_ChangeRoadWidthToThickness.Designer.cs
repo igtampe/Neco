85fff14a098bf906ada5a1a185ae34691b910f7f
Ignacio Tampe <Igtampe@gmail.com>
2021-09-24 15:08:54 -0400

    Migrate Remove incomItemID and Rename RoadWidth to Road Thickness

--- Neco.Data/Migrations/20210924190624_ChangeRoadWidthToThickness.Designer.cs
+++ Neco.Data/Migrations/20210924190624_ChangeRoadWidthToThickness.Designer.cs
@@ -4,14 +4,19 @@ using Igtampe.Neco.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Igtampe.Neco.Data.Migrations
 {
+    /// <summary></summary>
     [DbContext(typeof(NecoContext))]
-    partial class NecoContextModelSnapshot : ModelSnapshot
+    [Migration("20210924190624_ChangeRoadWidthToThickness")]
+    partial class ChangeRoadWidthToThickness
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <summary></summary>
+        /// <param name="modelBuilder"></param>
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Neco.Data/Migrations/20210924190624_ChangeRoadWidthToThickness.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Igtampe.Neco.Data.Migrations
+{
+    public partial class ChangeRoadWidthToThickness : Migration
+    {
+        /// <summary></summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Asset_IncomeItem_IncomeItemID",
+                table: "Asset");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Asset_IncomeItemID",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "IncomeItemID",
+                table: "Asset");
+
+            migrationBuilder.RenameColumn(
+                name: "Width",
+                table: "Road",
+                newName: "Thickness");
+        }
+
+        /// <summary></summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Thickness",
+                table: "Road",
+                newName: "Width");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IncomeItemID",
+                table: "Asset",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Asset_IncomeItemID",
+                table: "Asset",
+                column: "IncomeItemID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Asset_IncomeItem_IncomeItemID",
+                table: "Asset",
+                column: "IncomeItemID",
+                principalTable: "IncomeItem",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
