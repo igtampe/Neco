72d2d87bff88fc0198ef059f877082b11a819adb
Ignacio Tampe <Igtampe@gmail.com>
2021-09-28 14:20:53 -0400

    Add leftmostx and topmosty to landview item

--- Neco/LandView/ILandViewItem.cs
+++ Neco/LandView/ILandViewItem.cs
@@ -5,7 +5,16 @@ namespace Igtampe.Neco.Common.LandView {
     /// <summary>Interface that defines LandViewItems that are drawable on screen</summary>
     public interface ILandViewItem : System.ICloneable {
         
+        /// <summary>Graphical points that represent a polygon for this landview item</summary>
         public Point[] GraphicalPoints { get; }
+
+        /// <summary>Gets this landviewitem's leftmost X coordinate</summary>
+        /// <returns></returns>
+        public int LeftmostX();
+
+        /// <summary>Gets this landviewitem's Topmost y coordinate</summary>
+        /// <returns></returns>
+        public int TopmostY();
 
         /// <summary>Width of this item</summary>
         /// <returns></returns>
