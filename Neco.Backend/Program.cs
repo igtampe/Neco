0d5d42f95d94eef6f7d5e535033de3411e0c900d
Ignacio Tampe <Igtampe@gmail.com>
2021-09-28 13:58:54 -0400

    Backend now runs Session remover thread

--- Neco.Backend/Program.cs
+++ Neco.Backend/Program.cs
@@ -5,12 +5,19 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Igtampe.Neco.Backend {
     public static  class Program {
         
-        public static void Main(string[] args) { CreateHostBuilder(args).Build().Run(); }
+        public static void Main(string[] args) {
+
+            //Start session remover thread
+            new Thread(delegate () { SessionManager.SessionRemoverThread(60); }).Start();
+
+            //Start the website
+            CreateHostBuilder(args).Build().Run(); 
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args) .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>();});
