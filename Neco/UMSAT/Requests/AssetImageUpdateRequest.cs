7024d97ab211443388111f27c08397093ca90e4d
Ignacio Tampe <Igtampe@gmail.com>
2021-09-29 21:05:03 -0400

    Add Asset Image Update Request

---
+++ Neco/UMSAT/Requests/AssetImageUpdateRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Igtampe.Neco.Common.Requests;
+
+namespace Igtampe.Neco.Common.UMSAT.Requests {
+    /// <summary>Request to update the image of an asset</summary>
+    public class AssetImageUpdateRequest:INecoRequest {
+
+        /// <summary>ID of the session executing this request</summary>
+        public Guid SessionID { get; set; }
+
+        /// <summary>ID of the Asset to add this image to</summary>
+        public Guid AssetID { get; set; }
+
+        /// <summary>Image as a byte array that should be a png</summary>
+        public byte[] Image { get; set; }
+
+    }
+}
