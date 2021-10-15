60e820338a3938dd5b58ce7920dbe3a6e7c3e962
Ignacio Tampe <Igtampe@gmail.com>
2021-10-14 20:59:56 -0400

    Better handling of connection string file

--- Neco.Data/Constants.cs
+++ Neco.Data/Constants.cs
@@ -5,14 +5,15 @@ namespace Igtampe.Neco.Data {
 
         private static string ConString;
 
+        private const string ConStringFile = "ConString.txt";
+        private const string DefaultConString = @"Data Source=Localhost;Initial Catalog=Neco;Integrated Security=True";
+
         /// <summary>Connection string used by all contexts</summary>
         internal static string ConnectionString { 
-            
+
             get {
-                if (string.IsNullOrEmpty(ConString)) {
-                    if (File.Exists("ConString.txt")) { ConString = File.ReadAllText("ConString.txt"); } 
-                    else { ConString= @"Data Source=Localhost;Initial Catalog=Neco;Integrated Security=True"; }
-                }
+                if (!File.Exists(ConStringFile)) { File.WriteAllText(ConStringFile,DefaultConString); }
+                if (string.IsNullOrEmpty(ConString)) {ConString = File.ReadAllText(ConStringFile);}
                 return ConString;
             } 
             
