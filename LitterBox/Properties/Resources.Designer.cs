3dc70cb677c120d2af0036af59a58687ef456851
Ignacio Tampe <Igtampe@gmail.com>
2021-10-20 18:09:56 -0400

    Add WarnIcon

--- LitterBox/Properties/Resources.Designer.cs
+++ LitterBox/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace Igtampe.LitterBox.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap WarnIcon {
+            get {
+                object obj = ResourceManager.GetObject("WarnIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
