278ff7c42e7ccdfd8b0fc78c9b71752704e2551e
Ignacio Tampe <Igtampe@gmail.com>
2021-11-22 12:35:19 -0500

    New Bank Form for creating banks

---
+++ LitterBox/ItemForms/NewBankForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Igtampe.LitterBox.ItemForms {
+    partial class NewBankForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IDBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.OKBTN = new System.Windows.Forms.Button();
+            this.CancelBTN = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.IDBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.NameBox, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.OKBTN, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.CancelBTN, 1, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(431, 220);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 2);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(20, 20, 20, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(391, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID (Short Name) for this bank  (5 chars)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // IDBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.IDBox, 2);
+            this.IDBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.IDBox.Location = new System.Drawing.Point(20, 50);
+            this.IDBox.Margin = new System.Windows.Forms.Padding(20, 10, 20, 10);
+            this.IDBox.MaxLength = 5;
+            this.IDBox.Name = "IDBox";
+            this.IDBox.Size = new System.Drawing.Size(391, 23);
+            this.IDBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label2, 2);
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(20, 93);
+            this.label2.Margin = new System.Windows.Forms.Padding(20, 10, 20, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(391, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name for this Bank";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // NameBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.NameBox, 2);
+            this.NameBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.NameBox.Location = new System.Drawing.Point(20, 133);
+            this.NameBox.Margin = new System.Windows.Forms.Padding(20, 10, 20, 10);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(391, 23);
+            this.NameBox.TabIndex = 3;
+            // 
+            // OKBTN
+            // 
+            this.OKBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.OKBTN.Location = new System.Drawing.Point(255, 176);
+            this.OKBTN.Margin = new System.Windows.Forms.Padding(3, 10, 3, 20);
+            this.OKBTN.Name = "OKBTN";
+            this.OKBTN.Size = new System.Drawing.Size(75, 23);
+            this.OKBTN.TabIndex = 4;
+            this.OKBTN.Text = "OK";
+            this.OKBTN.UseVisualStyleBackColor = true;
+            this.OKBTN.Click += new System.EventHandler(this.OKBTN_Click);
+            // 
+            // CancelBTN
+            // 
+            this.CancelBTN.Location = new System.Drawing.Point(336, 176);
+            this.CancelBTN.Margin = new System.Windows.Forms.Padding(3, 10, 20, 20);
+            this.CancelBTN.Name = "CancelBTN";
+            this.CancelBTN.Size = new System.Drawing.Size(75, 23);
+            this.CancelBTN.TabIndex = 5;
+            this.CancelBTN.Text = "Cancel";
+            this.CancelBTN.UseVisualStyleBackColor = true;
+            this.CancelBTN.Click += new System.EventHandler(this.CancelBTN_Click);
+            // 
+            // NewBankForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 220);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "NewBankForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New Bank";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IDBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button OKBTN;
+        private System.Windows.Forms.Button CancelBTN;
+    }
+}
---
+++ LitterBox/ItemForms/NewBankForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Igtampe.Neco.Common;
+
+namespace Igtampe.LitterBox.ItemForms {
+    public partial class NewBankForm : Form {
+
+        public Bank ReturnBank { get; set; }
+
+        public NewBankForm() { InitializeComponent(); }
+
+        private void CancelBTN_Click(object sender, EventArgs e) { Close(); }
+
+        private void OKBTN_Click(object sender, EventArgs e) {
+            if (IDBox.Text.Length != 5) {
+                MessageBox.Show("ID of Bank must be 5 characters","No",MessageBoxButtons.OK,MessageBoxIcon.Hand); //When did MessageboxIcon Hand get added?
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(IDBox.Text) || string.IsNullOrWhiteSpace(NameBox.Text)) {
+                MessageBox.Show("Make sure to fill in all details pls", "No", MessageBoxButtons.OK, MessageBoxIcon.Hand); //When did MessageboxIcon Hand get added?
+                return;
+            }
+
+            ReturnBank = new() {
+                ID = IDBox.Text,
+                Name = NameBox.Text,
+                Accounts = new(),
+                AccountTypes = new()
+            };
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
