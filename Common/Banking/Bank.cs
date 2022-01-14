4538717a559f0221e2167599dde61ae2a835ba54
Ignacio Tampe <Igtampe@gmail.com>
2022-01-14 12:43:35 -0400

    Banking

---
+++ Common/Banking/Bank.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Igtampe.Neco.Common.Banking {
+
+    /// <summary>A representation of a financial institution</summary>
+    public class Bank : Identifiable<string>, Nameable, Depictable {
+
+        /// <summary>Name of this bank</summary>
+        public string Name { get; set; } = "";
+
+        /// <summary>Image URL of the logo of this bank</summary>
+        public string ImageURL { get; set; } = "";
+
+        /// <summary>Accounts with this Bank</summary>
+        [JsonIgnore]
+        public List<Account> Accounts { get; set; } = new();
+    }
+}
---
+++ Common/Banking/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Igtampe.Neco.Common.Banking {
+
+    /// <summary>A Transaction between two bank accounts</summary>
+    public class Transaction : AutomaticallyGeneratableIdentifiable, Nameable, Certifiable {
+
+        /// <summary>Origin of this transaction</summary>
+        public Account? Origin { get; set; }
+
+        /// <summary>Destination of this transaction</summary>
+        public Account? Destination { get; set; }
+
+        /// <summary>Amount transfered in this transaction</summary>
+        [Range(0,int.MaxValue)]
+        public long Amount { get; set; } = 0;
+
+        /// <summary>Date and time this transaction was executed on</summary>
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        /// <summary>Name of this transaction</summary>
+        public string Name { get; set; } = "";
+
+        /// <summary>Generates a certification for this transaction</summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public CertifiedItem GenerateCertification(User Certifier) => new() { CertifiedBy = Certifier, Date = DateTime.Now, Text = $"{Name}: Transfered {Amount:n0} from {Origin?.ID} to {Destination?.ID}" };
+
+    }
+}
