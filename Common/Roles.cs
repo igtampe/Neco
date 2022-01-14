7f396d7577d2a096dba0c52f55824456a2116fee
Ignacio Tampe <Igtampe@gmail.com>
2022-01-14 12:42:38 -0400

    User, Roles, and Notifications

---
+++ Common/Roles.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Igtampe.Neco.Common {
+    
+    /// <summary>User roles</summary>
+    public class Roles : AutomaticallyGeneratableIdentifiable {
+
+        /// <summary>Whether or not this user is a Neco Admin</summary>
+        public bool Admin { get; set; } = false;
+
+        /// <summary>Whether or not this user is a Government Official</summary>
+        public bool Government { get; set; } = false;
+
+        /// <summary>Whether or not this user is a member of the Salary Determination Committee</summary>
+        public bool SDC { get; set; } = false;
+
+        /// <summary>Whether or not this user is allowed to upload images to the server</summary>
+        public bool ImageUploader { get; set; } = false;
+
+    }
+}
---
+++ Common/User.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+using Igtampe.Neco.Common.IDGenerators;
+using Igtampe.Neco.Common.Banking;
+
+namespace Igtampe.Neco.Common {
+
+    /// <summary>A Neco User</summary>
+    public class User : ManuallyGeneratableIdentifiable<string>, Nameable, Depictable {
+
+        private readonly static IDGenerator<string> Gen = new NumericalGenerator(5);
+
+        /// <summary>Name of this user</summary>
+        public string Name { get; set; } = "";
+
+        /// <summary>Profile picture of this user</summary>
+        public string ImageURL { get; set; } = "";
+
+        /// <summary>Password of this User</summary>
+        [JsonIgnore]
+        public string Password { get; set; } = "";
+
+        /// <summary>Roles of this user</summary>
+        public Roles Roles { get; set; } = new();
+
+        //-[Banking]-
+
+        /// <summary>List of all accounts this person holds</summary>
+        [JsonIgnore]
+        public List<Account> Accounts { get; set; } = new();
+
+        /// <summary>Notifications this user has</summary>
+        [JsonIgnore]
+        public List<Notification> Notifications { get; set; } = new();
+
+        //-[Overrides]-
+
+        /// <summary>Generator for a UserID </summary>
+        public override IDGenerator<string> IDGenerator => Gen;
+    }
+}
