de1bdb27b7c0cfdfe0d6a3adf3f1fde23c3e1a94
Ignacio Tampe <Igtampe@gmail.com>
2022-01-14 12:50:00 -0400

    User Controller
    Taken somewhat from Clothespin

---
+++ API/Requests/User/ResetPasswordRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Igtampe.Neco.API.Requests {
+
+    /// <summary>Request to change a password</summary>
+    public class ResetPasswordRequest {
+
+        /// <summary>User ID of the user to reset the password to</summary>
+        public string? User { get; set; }
+
+        /// <summary>New Password</summary>
+        public string? New { get; set; }
+    }
+}
