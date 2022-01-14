d0153e83339b8fc8de2f2dc8beffb4be9d432614
Ignacio Tampe <Igtampe@gmail.com>
2022-01-14 12:43:54 -0400

    Income items

---
+++ Common/Income/Corporation.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Igtampe.Neco.Common.Income {
+
+    /// <summary>Corporation that generates monthly income</summary>
+    public class Corporation : IncomeItem {
+
+        /// <summary>US population (Useful in determining the base percentage for income calculation)</summary>
+        private const long US_POPULATION = 329500000;
+
+        /// <summary>Name of the real life equivalent company to base this on</summary>
+        public string RLE { get; set; } = "";
+
+        /// <summary>Net yearly income of the real life equivalent (In US Dollars)</summary>
+        [Range(0, long.MaxValue)]
+        public long RLENetYearly { get; set; } = 0;
+
+        /// <summary>Additional buildings this company contains</summary>
+        [Range(0, int.MaxValue)]
+        public int Buildings { get; set; } = 0;
+
+        /// <summary>Company mergers</summary>
+        [Range(0, int.MaxValue)]
+        public int Mergers { get; set; } = 0;
+
+        /// <summary>Whether or not this company has purchased metro ads</summary>
+        public bool MetroAds { get; set; } = false;
+
+        /// <summary>Whether or not this company has purchased airport ads</summary>
+        public bool AirportAds { get; set; } = false;
+
+        /// <summary>Whether or not this company is a multinational</summary>
+        public bool International { get; set; }
+
+        /// <summary>Whether or not this filing has been approved by the Salary Determination Committee</summary>
+        public bool Approved { get; set; } = false;
+
+        /// <summary>Gets corporate income percentage of RLE</summary>
+        /// <returns></returns>
+        protected virtual double GetIncomePercentage() {
+            double? BasePercentage = (Jurisdiction?.GetTopParent().Population * 1.0) / US_POPULATION * 100;
+            
+            //Buildings and Mergers
+            if (RLENetYearly > 10000000000) {
+                BasePercentage += .0005 * Buildings;
+                BasePercentage += .001 * Mergers;
+            } else if (RLENetYearly > 3000000000) { 
+                BasePercentage += .001 * Buildings;
+                BasePercentage += .0015 * Mergers;
+            } else { 
+                BasePercentage += .0015 * Buildings;
+                BasePercentage += .002 * Mergers;
+            }
+
+            //Other boosters
+            if (MetroAds) { BasePercentage += .001; }
+            if(AirportAds) { BasePercentage += .003; }
+            if (International) { BasePercentage += .005; }
+
+            return BasePercentage ?? 0;
+
+        }
+
+        /// <summary>Gets the Income of this corporation</summary>
+        /// <returns></returns>
+        public override long Income() => base.Income() + Convert.ToInt64(GetIncomePercentage() * (RLENetYearly / 12));
+    }
+}
---
+++ Common/Income/IncomeItem.cs
@@ -0,0 +1,40 @@
+﻿using Igtampe.Neco.Common.Taxes;
+using Igtampe.Neco.Common.Banking;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace Igtampe.Neco.Common.Income {
+
+    /// <summary>Basic usable, but extendable income item</summary>
+    public abstract class IncomeItem : AutomaticallyGeneratableIdentifiable, Nameable, Describable, Locatable, Certifiable {
+
+        /// <summary>Name of the income item</summary>
+        public string Name { get; set; } = "";
+        
+        /// <summary>Description of the income item</summary>
+        public string Description { get; set; } = "";
+
+        /// <summary>Address of this income item</summary>
+        public string? Address { get; set; }
+
+        /// <summary>District of this income item</summary>
+        public Jurisdiction? Jurisdiction { get; set; }
+
+        /// <summary>Account where this income will be deposited to</summary>
+        [JsonIgnore]
+        public Account? Account { get; set; }
+
+        /// <summary>Miscellaneous income of this item</summary>
+        [Range(0, long.MaxValue)]
+        public long MiscIncome { get; set; } = 0;
+
+        /// <summary>Creates a certification for this income item</summary>
+        /// <param name="Certifier"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public CertifiedItem GenerateCertification(User Certifier) => new() { CertifiedBy = Certifier, Date = DateTime.Now, Text = $"{Name} was certified with {Income():n0} Income" };
+
+        /// <summary>Total income of this item</summary>
+        public virtual long Income() => MiscIncome;
+    }
+}
