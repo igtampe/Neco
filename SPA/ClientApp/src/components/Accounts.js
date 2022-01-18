0288a7b4b2c4ea9834062a9744fd15580529ac67
Ignacio Tampe <Igtampe@gmail.com>
2022-01-18 01:23:53 -0400

    Add the neco header to the accounts page, layout, some potential quick actions

--- SPA/ClientApp/src/components/Accounts.js
+++ SPA/ClientApp/src/components/Accounts.js
@@ -1,8 +1,45 @@
+import { Card, CardContent, CircularProgress, Divider, Grid, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import React from "react";
+import NecoHeader from "./NecoHeader";
 
-export default function AccountsComponent() {
+export default function AccountsComponent(props) {
+
+    if(!props.User) {return (<CircularProgress/>)}
+
+    console.log(props)
+
     return (
         <React.Fragment>
+            <NecoHeader name={props.User ? props.User.name : '...'} />
+            <Grid container spacing={2} style={{ minWidth: '100%' }}>
+                <Grid item xs={9}>
+
+                    Accordion for each account that in the summary has Bank name, Account Name, Balance
+                    and in the details includes 3 latest transactions and buttons to:
+
+                    Menu with:
+                        see more transactions
+                        Manage owners
+                        Close account
+                    
+                    Send Money (big)
+
+                </Grid>
+                <Grid item xs={3}>
+                    <Card sx={{ minWidth: '100%', maxHeight:'100%', height:'60vh' }}>
+                        <CardContent>
+                            <b>Quick actions</b>
+                            <Divider />
+                            <List>
+                                <ListItem button> Add an Account </ListItem>
+                                <ListItem button> Send a Check </ListItem>
+                                <ListItem button> Send a Bill </ListItem>
+                                <ListItem button> Add an Income Item </ListItem>
+                            </List>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </React.Fragment>
     );
 
