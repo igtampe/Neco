6298279bcfd18f2a387fbf308e3c43a5941576f0
Ignacio Tampe <Igtampe@gmail.com>
2022-01-18 01:22:37 -0400

    More or less finish the home page
    (Just need some photos of the app in operation once it's ready)

--- SPA/ClientApp/src/components/Home.js
+++ SPA/ClientApp/src/components/Home.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
+import { Button, Card, CardContent, Divider, Grid, Typography } from '@mui/material';
+import React from 'react';
+import { useHistory } from "react-router-dom";
 
-export class Home extends Component {
-  static displayName = Home.name;
-
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+function AdCard(props) {
+  return (
+  <Card sx={{ minWidth: '100%' }}>
+    <CardContent>
+      <img src={props.img} width='100%' alt=''/>
+      <Typography gutterBottom sx={{textAlign:'center', marginTop:'10px'}}> <h4>{props.title}</h4> </Typography>
+      <Divider />
+      <div style={{ minWidth: '100%', maxHeight: '200px', overflowY: 'auto' }} >
+        {props.children}
       </div>
-    );
-  }
+    </CardContent>
+  </Card>)
+}
+
+export default function Home(props) {
+  const history = useHistory();
+
+  return (
+    <div>
+
+      <div style={{ marginBottom: '15px', position: 'relative' }}>
+        <img src='/images/home.jpg' alt='Newpond Central' width='100%' /><br />
+        <div style={{ position: 'absolute', bottom: '50%', left: '50%', transform: 'translate(-50%,50%)', textShadow: '2px 2px 8px #000000' }}>
+          <img src='/necowhiteShadow.png' width='100%' alt='Neco Logo'/>
+        </div>
+      </div>
+
+      <h2 style={{ textAlign: 'center' }}>Welcome to the future of UMS Banking</h2>
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <Button onClick={()=>{history.push("/Login")}}variant={'contained'}> Get Started</Button>
+      </div>
+      <br />
+      <Grid container style={{ minWidth: '100%' }} spacing={2}>
+        <Grid item xs={props.Vertical ? 12 : 4}>
+          <AdCard title='Powerful' img='/necocolor.png'>
+            When complete, Neco will have all the features ViBE had, and more. With powerful tools to verify land ownership, easy management of any and all income items direclty on the site, live statistics on the economy, and much more.
+          </AdCard>
+        </Grid>
+
+        <Grid item xs={props.Vertical ? 12 : 4}>
+          <AdCard title='Flexible' img='/necocolor.png'>
+            Neco is built with flexibility in mind, allowing for future expansion. Banks and jurisdictions are easily createable, editable, and manageable from the admin dashboard.
+          </AdCard>
+        </Grid>
+
+        <Grid item xs={props.Vertical ? 12 : 4}>
+          <AdCard title='Easier' img='/necocolor.png'>
+            With a web frontend, sharable bank accounts, and easier and fully integrated income management, Neco makes it easy and intuitive to join the UMS Economy. It'll do what ViBE did with TEBECON all over again.
+          </AdCard>
+        </Grid>
+
+      </Grid>
+    </div>
+  );
 }
