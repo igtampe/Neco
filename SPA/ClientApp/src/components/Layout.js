d2f2954dbbe04157789f2038557fe8d3d043dc76
Ignacio Tampe <Igtampe@gmail.com>
2022-01-17 20:57:13 -0400

    Reorganize Layout

--- SPA/ClientApp/src/components/Layout.js
+++ SPA/ClientApp/src/components/Layout.js
@@ -6,7 +6,8 @@ export default function Layout(props) {
   
     return (
       <div>
-        <NavMenu DarkMode={props.DarkMode} ToggleDarkMode={props.ToggleDarkMode} Session={props.Session} InvalidSession={props.InvalidSession} setSession = {props.SetSession} RefreshUser = {props.RefreshUser} User={props.User} />
+        <NavMenu DarkMode={props.DarkMode} ToggleDarkMode={props.ToggleDarkMode} Session={props.Session} InvalidSession={props.InvalidSession} 
+        setSession = {props.SetSession} RefreshUser = {props.RefreshUser} User={props.User} />
         <Container> {props.children} </Container>
       </div>
     );
