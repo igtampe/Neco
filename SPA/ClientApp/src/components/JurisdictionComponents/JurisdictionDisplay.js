14d187d816b42a0d529ef14aef399701032ca945
Ignacio Tampe <Igtampe@gmail.com>
2022-01-28 01:55:28 -0400

    Jurisdiction display and editor

---
+++ SPA/ClientApp/src/components/JurisdictionComponents/JurisdictionDisplay.js
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import { Box, TextField, IconButton, Select, MenuItem, FormControl, InputLabel, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, CircularProgress, Paper } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import EditIcon from '@mui/icons-material/Edit'
+import AddIcon from '@mui/icons-material/Add'
+import { GenerateGet } from '../../RequestOptionGenerator'
+import JurisdictionEditor from "./JurisdictionEditor";
+
+export function JurisdictionTypeSelect(props) {
+    return (
+        <FormControl fullWidth style={{ margintop: "15px" }}>
+            <InputLabel fullWidth>Type</InputLabel>
+            <Select fullWidth label="Label" value={props.type} onChange={(event) => {
+                props.setType(event.target.value)
+                props.setCollection(undefined)
+            }}>
+                <MenuItem value={0}>Country</MenuItem>
+                <MenuItem value={1}>State</MenuItem>
+                <MenuItem value={2}>County</MenuItem>
+                <MenuItem value={3}>City</MenuItem>
+            </Select></FormControl>
+    )
+
+}
+
+function JurisdictionRow(props) {
+
+    //This is basically a component
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <TableRow>
+                <TableCell width={'90px'}>
+                    <a href={props.J.imageURL === "" ? "/Flag.png" : props.J.imageURL}>
+                        <img alt={'Flag'} src={props.J.imageURL === "" ? "/Flag.png" : props.J.imageURL} height={'50px'} /></a>
+                </TableCell>
+                <TableCell width={'90px'}>{props.J.id}</TableCell>
+                <TableCell>{props.J.name}</TableCell>
+                <TableCell>{!props.J.parentJurisdiction ? "None" : props.J.parentJurisdiction.id + ": " + props.J.parentJurisdiction.name }</TableCell>
+                <TableCell width={'90px'}>
+
+                    {/** Edit Button. Potentially make the editor here too */}
+                    <IconButton onClick={() => { setOpen(true)}}><EditIcon /></IconButton>
+
+                </TableCell>
+            </TableRow>
+
+            <JurisdictionEditor jurisdiction={props.J} setJurisdictions={props.setJurisdictions} open={open} setOpen={setOpen} Session={props.Session}/>
+
+        </>
+    )
+
+
+}
+
+export default function JurisdictionDisplay(props) {
+
+    const [query, setQuery] = useState("");
+    const [type, setType] = useState(0)
+
+    const [jurisdictions, setJurisdictions] = useState(undefined)
+    const [loading, setLoading] = useState(false);
+
+    const [displayedItems, setDisplayedItems] = useState(0)
+    const [noMas, setNoMas] = useState(false)
+
+    const [newOpen, setNewOpen] = useState(false)
+
+    const startSearch = (event) => { setJurisdictions(undefined) }
+
+    //OK now 
+    if (!jurisdictions && !loading) {
+
+        setLoading(true)
+
+        var URL = '/API/Taxes/Jurisdictions?Type=' + type
+        if (query !== "") { URL = URL + '&Query=' + query }
+
+        fetch(URL, GenerateGet(props.Session)) //This actually isn't authenticated pero sabes que zoop.
+            .then(response => { return (response.json()) })
+            .then(data => {
+
+                //if there was an error then oops
+                if (data.error) { return; }
+
+                setJurisdictions(data)
+                setLoading(false)
+
+                setDisplayedItems(data.length)
+                setNoMas(data.length != 20)
+
+            })
+
+    }
+
+
+    return (
+        <React.Fragment>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <TextField fullWidth label="Search" value={query} onChange={(event) => { setQuery(event.target.value) }} />
+                {props.Vertical ? <></> :
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }} style={{ marginLeft: "25px" }}>
+                        <JurisdictionTypeSelect type={type} setType={setType} setCollection={setJurisdictions} /> </Box>
+                }
+                <IconButton onClick={startSearch} style={{ marginLeft: '10px', marginBottom: '7px' }}><SearchIcon /></IconButton>
+                <IconButton onClick={() => { setNewOpen(true)}} style={{ marginBottom: '7px' }}><AddIcon /></IconButton>
+            </Box>
+            {props.Vertical ?
+                <Box sx={{ display: 'flex', alignItems: 'flex-end' }} style={{ marginTop: "25px" }}>
+                    <JurisdictionTypeSelect type={type} setType={setType} setCollection={setJurisdictions} />
+                </Box>
+                : <></>}
+
+            <TableContainer component={Paper} style={{ marginTop: '25px' }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Flag</TableCell>
+                            <TableCell>ID</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Parent</TableCell>
+                            <TableCell>Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            !jurisdictions ?
+                                <TableRow>
+                                    <TableCell colSpan={5} style={{ textAlign: "center" }}><CircularProgress /></TableCell>
+                                </TableRow> : <>{
+
+                                    jurisdictions.length === 0 ?
+                                        <TableRow>
+                                            <TableCell colSpan={5} style={{ textAlign: "center" }}>No jurisdictions</TableCell>
+                                        </TableRow> : <>{ jurisdictions.map(J => { return (<JurisdictionRow J={J} setJurisdictions={setJurisdictions} Session={props.Session}/>)})}</>
+                                }
+                                </>
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <JurisdictionEditor setJurisdictions={setJurisdictions} open={newOpen} setOpen={setNewOpen} Session={props.Session}/>
+
+        </React.Fragment>
+    );
+
+}
---
+++ SPA/ClientApp/src/components/JurisdictionComponents/JurisdictionEditor.js
@@ -0,0 +1,225 @@
+import React, { useState } from "react";
+import {
+    Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, FormControl, Grid,
+    InputLabel, MenuItem, Select, TextField
+} from "@mui/material";
+import AlertSnackbar from "../AlertSnackbar";
+import PicturePicker from "../PicturePicker";
+import { CircularProgress } from "@mui/material";
+import { GenerateGet } from "../../RequestOptionGenerator";
+import { JurisdictionTypeSelect } from "./JurisdictionDisplay";
+import BracketDisplay from "./BracketDisplay";
+
+export function JurisdictionSelect(props) {
+
+    const [loading, setLoading] = useState(false);
+
+    if (!props.jurisdictions && props.setJurisdictions && !loading && props.type && props.type !== 0) {
+        //if we don't have jurisdictions and we can set jurisdictions and we're not loading
+        //AND we have a type *A N D* type is not 0
+
+        setLoading(true)
+
+        var URL = '/API/Taxes/Jurisdictions?Type=' + (props.type-1)
+
+        fetch(URL, GenerateGet(props.Session)) //This actually isn't authenticated pero sabes que zoop.
+            .then(response => { return (response.json()) })
+            .then(data => {
+
+                //if there was an error then oops
+                if (data.error) { return; }
+
+                props.setJurisdictions(data)
+                setLoading(false)
+            })
+
+    }
+
+    if(props.type===0){return (<></>)}
+    if(!props.jurisdictions){ return(<CircularProgress/>) }
+
+    return (
+        <>
+            <FormControl fullWidth style={{ margintop: "15px" }} disabled={props.type===0}>
+                <InputLabel fullWidth>Parent</InputLabel>
+                <Select fullWidth label="Label" value={props.Jurisdiction} onChange={(event) => { props.setJurisdictionID(event.target.value) }}>
+                    { props.jurisdictions.map( j=>{ return( <MenuItem value={j.id}>{j.id}: {j.name}</MenuItem> )}) }
+                </Select></FormControl>
+        </>
+
+    )
+
+}
+
+export default function JurisdictionEditor(props) {
+
+    const [name, setName] = useState("");
+    const [imageURL, setImageURL] = useState("");
+
+    const [parentID, setParentID] = useState("");
+    const [accountID, setAccountID] = useState("");
+    const [population, setPopulation] = useState(0);
+
+    const [type, setType] = useState(0);
+    const [jurisdictions, setJurisdictions] = useState(undefined)
+
+    const [pickerOpen, setPickerOpen] = useState(false)
+
+    const [populated, setPopulated] = useState(false);
+
+    const [result, setResult] = useState({
+        severity: "success",
+        text: "idk"
+    })
+
+    const [SnackOpen, setSnackOpen] = useState(false);
+
+    if (props.jurisdiction && !populated && props.open) {
+        //Populate
+        setPopulated(true);
+        setName(props.jurisdiction.name)
+        setImageURL(props.jurisdiction.imageURL)
+        setType(props.jurisdiction.type)    
+        setPopulation(props.jurisdiction.population)
+        if (props.jurisdiction.parentJurisdiction) { setParentID(props.jurisdiction.parentJurisdiction.id) }
+    }
+
+    const ClearForm = () => {
+        setName("")
+        setImageURL("")
+        setType(0)
+        setPopulation(0)
+
+        setPopulated(false)
+    }
+
+    const handleClosing = (event) => {
+
+        props.setOpen(false)
+        ClearForm();
+    }
+
+    const handleOK = (event) => {
+
+        if (name === "") {
+            setResult({ severity: "danger", text: "Name cannot be empty!" })
+            setSnackOpen(true);
+        }
+
+        if (parentID === "" && type !== 0) {
+            setResult({ severity: "danger", text: "Must set a parent for non-country jurisdiction" })
+            setSnackOpen(true);
+        }
+
+        //Send the request 
+        var requestOptions;
+        var url;
+
+        if (props.jurisdiction) {
+            requestOptions = { method: 'PUT' };
+            url = "API/Taxes/Jurisdiction/" + props.jurisdiction.id
+        } else {
+            requestOptions = { method: 'POST', };
+            url = "API/Taxes/Jurisdiction"
+        }
+
+        requestOptions = {
+            ...requestOptions,
+            headers: { 'Content-Type': 'application/json', 'SessionID': props.Session },
+            body: JSON.stringify({
+                type: type, name: name, population: population,
+                imageURL: imageURL, parentJurisdictionID: parentID, 
+                tiedAccountID:accountID
+            })
+        }
+        
+        console.log(requestOptions.body)
+
+        fetch(url,requestOptions)
+            .then(response => { return response.json() })
+            .then(data => {
+                if (data.error) {
+
+                    setResult({
+                        severity: "danger",
+                        text: "Could not " + (props.jurisdiction ? "update" : "create") + " this jurisdiction: " + data.reason
+                    })
+
+                    setSnackOpen(true);
+
+                } else {
+                    setResult({
+                        severity: "success",
+                        text: name + " has been " + (props.jurisdiction ? "updated" : "created") + "!"
+                    })
+
+                    if (props.setJurisdictions) { props.setJurisdictions(undefined) }
+                    if (props.setJurisdiction) { props.setJurisdiction(data) }
+
+                    setSnackOpen(true);
+
+                    handleClosing();
+                }
+            })
+
+    }
+
+    return (
+        <React.Fragment>
+            <Dialog fullWidth maxWidth="md" open={props.open} onClose={handleClosing}>
+                <DialogTitle>{props.jurisdiction ? 'Edit a Jurisdiction' : 'Create a new Jurisdiction'}</DialogTitle>
+                <DialogContent>
+                    <table style={{ width: "100%" }}>
+                        <tr>
+                            <td>
+                                <TextField label="Name" value={name} onChange={(event) => setName(event.target.value)} fullWidth
+                                    style={{ marginTop: "5px", marginBottom: "5px" }} />                            </td>
+                            <td rowSpan="2" style={{ width: "135px" }}>
+                                <Button onClick={() => setPickerOpen(true)}>
+                                    <img src={imageURL === "" ? "/flag.png" : imageURL} alt="Flag" width="100px" style={{ marginLeft: "25px", marginRight: "10px" }} />
+                                </Button>
+                            </td>
+                        </tr>
+                    </table>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <TextField label="Population" value={population} onChange={(event) => setPopulation(event.target.value)} fullWidth
+                                style={{ marginTop: "5px", marginBottom: "5px" }} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField label="Tied Neco Bank Account" value={accountID} onChange={(event) => setAccountID(event.target.value)} fullWidth
+                                style={{ marginTop: "5px", marginBottom: "5px" }} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <JurisdictionTypeSelect type={type} setType={setType} setCollection={setJurisdictions} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <JurisdictionSelect type={type} setJurisdictionID={setParentID} jurisdictions={jurisdictions} setJurisdictions={setJurisdictions}
+                            Jurisdiction={parentID}/>
+                        </Grid>
+                    </Grid>
+
+                    {
+                        !props.jurisdiction ? <></> : <>
+                            <Divider style={{marginTop:'15px', marginBottom:'15px'}}/>    
+                            <BracketDisplay JurisdictionID={props.jurisdiction.id} Session={props.Session}/>
+                        
+                        </>
+                    }
+                    
+
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleOK}>OK</Button>
+                    <Button onClick={handleClosing}>Cancel</Button>
+                </DialogActions>
+            </Dialog>
+
+            <AlertSnackbar open={SnackOpen} setOpen={setSnackOpen} result={result} />
+
+            <PicturePicker open={pickerOpen} setOpen={setPickerOpen} imageURL={imageURL} setImageURL={setImageURL} defaultImage={"/flag.png"} />
+
+        </React.Fragment>
+    );
+
+}
