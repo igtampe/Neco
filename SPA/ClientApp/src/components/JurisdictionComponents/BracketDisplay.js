089e1fafa0bff26986165c35751e7716cd6bc5b7
Ignacio Tampe <Igtampe@gmail.com>
2022-01-28 01:55:18 -0400

    Bracket display and editor

---
+++ SPA/ClientApp/src/components/JurisdictionComponents/BracketDisplay.js
@@ -0,0 +1,128 @@
+import { Button, CircularProgress, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import React, { useState } from "react";
+import EditIcon from '@mui/icons-material/Edit'
+import DelIcon from '@mui/icons-material/Delete'
+import BracketEditor from "./BracketEditor";
+import DeleteConfirm from "../Subcomponents/DeleteConfirm";
+import { GenerateDelete } from "../../RequestOptionGenerator";
+
+export const IncomeTypes = [ "Personal","Corporate","Government", "Charity" ]
+
+function BracketRow(props){
+
+    //This is basically a component
+    const [open, setOpen] = useState(false);
+    const [delConfirm, setDelConfirm] = useState(false);
+
+    const [deleting, setDeleting] = useState(false)
+
+    const RealDelete = () => {
+
+        setDeleting(true)
+
+        //send the fetch y lo que sea
+        fetch('API/Taxes/Brackets/' + props.B.id,GenerateDelete(props.Session))
+        .then(r=>r.json())
+        .then(data=>{
+
+            setDeleting(false)
+            if(data.error) {return;}            
+            props.setBrackets(undefined)
+
+        })
+
+    }
+
+    return (
+        <>
+            <TableRow>
+                <TableCell width={'150px'}>{props.B.name}</TableCell>
+                <TableCell width={'90px'}>{(props.B.rate.toLocaleString('en-US',{style:'percent'}))}</TableCell>
+                <TableCell width={'120px'}>{IncomeTypes[props.B.incomeType]}</TableCell>
+                <TableCell>{props.B.start.toLocaleString()}p</TableCell>
+                <TableCell>{props.B.end.toLocaleString()}p</TableCell>
+                <TableCell width={'120px'}>
+
+                    { 
+                        deleting ? <> <CircularProgress/> </>
+                        : <>
+                            <IconButton onClick={() => { setOpen(true)}}><EditIcon /></IconButton>
+                            <IconButton onClick={() => { setDelConfirm(true)}}><DelIcon /></IconButton>
+                        </>
+                    }
+
+                </TableCell>
+            </TableRow>
+
+            <BracketEditor bracket={props.B} setBrackets={props.setBrackets} open={open} setOpen={setOpen} Session={props.Session} {...props}/>
+            
+            <DeleteConfirm open={delConfirm} setOpen={setDelConfirm} delete={RealDelete}>
+                Are you sure you want to delete this bracket?
+            </DeleteConfirm>
+
+        </>
+    )
+
+}
+
+export default function BracketDisplay(props){
+
+    const [brackets, setBrackets] = useState(undefined)
+    const [loading, setLoading] = useState(false)
+
+    const [newOpen, setNewOpen] = useState(false);
+
+    if(props.JurisdictionID && !brackets && !loading){
+
+        //Time to load
+        setLoading(true)
+
+        fetch('/API/Taxes/Jurisdiction/' + props.JurisdictionID + '/Brackets') //actually we can just fetch normally no?
+        .then(response=>response.json())
+        .then(data=>{
+
+            //if error, oops
+            if(data.error){ return; }
+
+            setBrackets(data)
+            setLoading(false)
+
+        })
+
+    }
+
+    return(<>
+    
+        <TableContainer component={Paper} style={{ marginTop: '25px' }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Rate</TableCell>
+                            <TableCell>Income Type</TableCell>
+                            <TableCell>Start</TableCell>
+                            <TableCell>End</TableCell>
+                            <TableCell>Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            !brackets ?
+                                <TableRow>
+                                    <TableCell colSpan={6} style={{ textAlign: "center" }}><CircularProgress /></TableCell>
+                                </TableRow> : <>
+                                    { brackets.map(B => { return (<BracketRow B={B} setBrackets={setBrackets} Session={props.Session} {...props}/>)})}
+                                    <TableRow><TableCell colSpan={6} style={{ textAlign: "center" }}>
+                                        <Button variant="contained" onClick={()=>{setNewOpen(true)}}> Add a new Tax Bracket</Button>
+                                    </TableCell></TableRow> 
+                                </>
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <BracketEditor setBrackets={setBrackets} open={newOpen} setOpen={setNewOpen} Session={props.Session} {...props}/>
+    
+    </>)
+
+}
---
+++ SPA/ClientApp/src/components/JurisdictionComponents/BracketEditor.js
@@ -0,0 +1,177 @@
+import React, { useState } from "react";
+import {
+    Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, FormControl, Grid,
+    InputLabel, MenuItem, Select, TextField
+} from "@mui/material";
+import AlertSnackbar from "../AlertSnackbar";
+import { CircularProgress } from "@mui/material";
+import { GenerateGet } from "../../RequestOptionGenerator";
+import BracketDisplay from "./BracketDisplay";
+
+export function IncomeTypeSelect(props) {
+    return (
+        <FormControl fullWidth style={{ margintop: "15px" }}>
+            <InputLabel fullWidth>Type</InputLabel>
+            <Select fullWidth label="Label" value={props.type} onChange={(event) => { props.setType(event.target.value) }}>
+                <MenuItem value={0}>Personal</MenuItem>
+                <MenuItem value={1}>Corporate</MenuItem>
+                {/**<MenuItem value={2}>Government</MenuItem>
+                <MenuItem value={3}>Charity</MenuItem>
+                These income types pay no taxes
+                **/}
+            </Select></FormControl>
+    )
+
+}
+
+
+export default function BracketEditor(props) {
+
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [rate, setRate] = useState(0.0)
+    const [incomeType, setIncomeType] = useState(0)
+    const [start, setStart] = useState(0)
+    const [end, setEnd] = useState(0)
+
+    const [populated, setPopulated] = useState(false);
+
+    const [result, setResult] = useState({
+        severity: "success",
+        text: "idk"
+    })
+
+    const [SnackOpen, setSnackOpen] = useState(false);
+
+    if (props.bracket && !populated && props.open) {
+        //Populate
+        setPopulated(true);
+        setName(props.bracket.name)
+        setDescription(props.bracket.description)
+        setRate(props.bracket.rate * 100)
+        setIncomeType(props.bracket.incomeType)
+        setStart(props.bracket.start)
+        setEnd(props.bracket.end)
+    }
+
+    const ClearForm = () => {
+        setName("")
+        setDescription("")
+        setRate(0)
+        setIncomeType(0)
+        setStart(0)
+        setEnd(0)
+
+        setPopulated(false)
+    }
+
+    const handleClosing = (event) => {
+
+        props.setOpen(false)
+        ClearForm();
+    }
+
+    const handleOK = (event) => {
+
+        if (name === "") {
+            setResult({ severity: "danger", text: "Name cannot be empty!" })
+            setSnackOpen(true);
+            return;
+        }
+
+        //Send the request 
+        var requestOptions;
+        var url;
+
+        if (props.bracket) {
+            requestOptions = { method: 'PUT' };
+            url = "API/Taxes/brackets/" + props.bracket.id
+        } else {
+            requestOptions = { method: 'POST', };
+            url = "API/Taxes/Brackets?Jurisdiction=" + props.JurisdictionID
+        }
+
+        requestOptions = {
+            ...requestOptions,
+            headers: { 'Content-Type': 'application/json', 'SessionID': props.Session },
+            body: JSON.stringify({
+                ...props.bracket,
+                name: name, description: description,
+                rate: (rate/100.0), incomeType:incomeType,
+                start: start, end: end
+            })
+        }
+
+        console.log(requestOptions.body)
+
+        fetch(url, requestOptions)
+            .then(response => { return response.json() })
+            .then(data => {
+                if (data.error) {
+
+                    setResult({
+                        severity: "danger",
+                        text: "Could not " + (props.jurisdiction ? "update" : "create") + " this jurisdiction: " + data.reason
+                    })
+
+                    setSnackOpen(true);
+
+                } else {
+                    setResult({
+                        severity: "success",
+                        text: name + " has been " + (props.jurisdiction ? "updated" : "created") + "!"
+                    })
+
+                    if (props.setBrackets) { props.setBrackets(undefined) }
+                    if (props.setBracket)  { props.setBracket(data) }
+
+                    setSnackOpen(true);
+
+                    handleClosing();
+                }
+            })
+
+    }
+
+    return (
+        <React.Fragment>
+            <Dialog fullWidth maxWidth="sm" open={props.open} onClose={handleClosing}>
+                <DialogTitle>{props.bracket ? 'Edit a Bracket' : 'Create a new Bracket'}</DialogTitle>
+                <DialogContent>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField label="Name" value={name} onChange={(event) => setName(event.target.value)} fullWidth
+                                style={{ marginTop: "5px", marginBottom: "5px" }} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <IncomeTypeSelect type={incomeType} setType={setIncomeType} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField label="Rate Percentage" value={rate} onChange={(event) => setRate(event.target.value)} fullWidth type='number'
+                                style={{ marginTop: "5px", marginBottom: "5px" }} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField label="Start Range" value={start} onChange={(event) => setStart(event.target.value)} fullWidth type='number'
+                                style={{ marginTop: "5px", marginBottom: "5px" }} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField label="End Range" value={end} onChange={(event) => setEnd(event.target.value)} fullWidth type='number'
+                                style={{ marginTop: "5px", marginBottom: "5px" }} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField label="Description/Notes" value={description} onChange={(event) => setDescription(event.target.value)} fullWidth multiline
+                                style={{ marginTop: "5px", marginBottom: "5px" }} maxRows={6} minRows={6} variant="filled" color="secondary" />
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleOK}>OK</Button>
+                    <Button onClick={handleClosing}>Cancel</Button>
+                </DialogActions>
+                <AlertSnackbar open={SnackOpen} setOpen={setSnackOpen} result={result} />
+            </Dialog>
+
+        </React.Fragment>
+    );
+
+}
