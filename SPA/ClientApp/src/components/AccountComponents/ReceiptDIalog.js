e166792b5dcd5516ffec8f633c609a3c68e5de1f
Ignacio Tampe <Igtampe@gmail.com>
2022-01-29 19:14:53 -0400

    Receipt dialog to show receipt images

---
+++ SPA/ClientApp/src/components/AccountComponents/ReceiptDIalog.js
@@ -0,0 +1,38 @@
+import { Dialog, DialogContent, DialogContentText, Grid, Alert } from "@mui/material";
+import React from "react";
+
+
+export default function ReceiptDialog(props) {
+
+    const handleClosing = () => { 
+        if(props.handleClosing) {props.handleClosing()}
+        if(props.setOpen) {props.setOpen(false) }
+    }
+
+    return (
+        <>
+            <Dialog maxWidth='sm' open={props.open} onClose={handleClosing}>
+                <DialogContent>
+                    <DialogContentText>
+                        <Grid container spacing={2}>
+                            {
+                                props.sent
+                                    ? <Grid item xs={12}>
+                                        <Alert severity={'success'}>Money sent! This receipt is viewable later by looking at your transactions</Alert>
+                                    </Grid>
+                                    : <></>
+
+                            }
+                            <Grid item xs={12} textAlign={'center'}>
+                                <img src={'/API/Cert/Transaction/' + props.transactionID} alt={'Receipt for transaction ' + props.transactionID} />
+                            </Grid>
+                        </Grid>
+                    </DialogContentText>
+                </DialogContent>
+
+            </Dialog>
+
+        </>
+    )
+
+}
