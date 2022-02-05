05ca143ad92079b5782a19e8580a1101f3446049
Ignacio Tampe <Igtampe@gmail.com>
2022-02-05 14:13:52 -0400

    Remove certified items (they were unused)

---
+++ Data/Migrations/20220205181314_removeCertified.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Igtampe.Neco.Data.Migrations
+{
+    public partial class removeCertified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CertifiedItem");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CertifiedItem",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    CertifiedByID = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CertifiedItem", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_CertifiedItem_User_CertifiedByID",
+                        column: x => x.CertifiedByID,
+                        principalTable: "User",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CertifiedItem_CertifiedByID",
+                table: "CertifiedItem",
+                column: "CertifiedByID");
+        }
+    }
+}
--- Data/NecoContext.cs
+++ Data/NecoContext.cs
@@ -55,8 +55,6 @@ namespace Igtampe.Neco.Data {
 
         public DbSet<Notification> Notification { get; set; }
 
-        public DbSet<CertifiedItem> CertifiedItems { get; set; }
-
         public DbSet<Image> Image { get; set; }
 
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
