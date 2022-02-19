915b825c7a18d669e91aad814c7474761e4cf5b7
Ignacio Tampe <Igtampe@gmail.com>
2022-02-19 13:23:00 -0400

    Add new home pictures

--- SPA/ClientApp/src/components/Home.js
+++ SPA/ClientApp/src/components/Home.js
@@ -6,7 +6,7 @@ function AdCard(props) {
   return (
   <Card sx={{ minWidth: '100%' }}>
     <CardContent>
-      <img src={props.img} width='100%' alt=''/>
+      <img src={props.img} width='100%' alt='' style={props.fit ? {height:'200px', objectFit:'cover'} : {}}/>
       <Typography gutterBottom sx={{textAlign:'center', marginTop:'10px'}}> <h4>{props.title}</h4> </Typography>
       <Divider />
       <div style={{ minWidth: '100%', maxHeight: '200px', overflowY: 'auto' }} >
@@ -36,19 +36,19 @@ export default function Home(props) {
       <br />
       <Grid container style={{ minWidth: '100%' }} spacing={2}>
         <Grid item xs={props.Vertical ? 12 : 4}>
-          <AdCard title='Powerful' img='/necocolor.png'>
+          <AdCard title='Powerful' img='/screenshots/necoshot1.png' fit={!props.Vertical}>
             When complete, Neco will have all the features ViBE had, and more. With powerful tools to verify land ownership, easy management of any and all income items direclty on the site, live statistics on the economy, and much more.
           </AdCard>
         </Grid>
 
         <Grid item xs={props.Vertical ? 12 : 4}>
-          <AdCard title='Flexible' img='/necocolor.png'>
+          <AdCard title='Flexible' img='/screenshots/necoshot4.png' fit={!props.Vertical}>
             Neco is built with flexibility in mind, allowing for future expansion. Banks and jurisdictions are easily createable, editable, and manageable from the admin dashboard.
           </AdCard>
         </Grid>
 
         <Grid item xs={props.Vertical ? 12 : 4}>
-          <AdCard title='Easier' img='/necocolor.png'>
+          <AdCard title='Easier' img='/screenshots/necoshot2.png' fit={!props.Vertical}>
             With a web frontend, sharable bank accounts, and easier and fully integrated income management, Neco makes it easy and intuitive to join the UMS Economy. It'll do what ViBE did with TEBECON all over again.
           </AdCard>
         </Grid>
