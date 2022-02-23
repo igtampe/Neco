70a4eb8c13c4788971bf485318f1c5218704ad89
Ignacio Tampe <Igtampe@gmail.com>
2022-02-23 13:49:38 -0400

    Link statistics components to the statistics pane

--- SPA/ClientApp/src/components/Statistics.js
+++ SPA/ClientApp/src/components/Statistics.js
@@ -1,9 +1,20 @@
 import React from "react";
+import IncomeStatistics from "./StatisticsComponents/IncomeStatistics";
+import { Divider } from "@mui/material";
+import TransactionStatistics from "./StatisticsComponents/TransactionStatistics";
+import WealthStatistics from "./StatisticsComponents/WealthStatistics";
+import TaxStatistics from "./StatisticsComponents/TaxStatistics";
 
-export default function StatisticsComponent() {
+export default function StatisticsComponent(props) {
     return (
         <React.Fragment>
-            (Figure out later)
+            <TransactionStatistics {...props}/>
+            <Divider/>
+            <WealthStatistics {...props}/>
+            <Divider/>
+            <IncomeStatistics {...props}/>
+            <Divider/>
+            <TaxStatistics {...props}/>
         </React.Fragment>
     );
 
