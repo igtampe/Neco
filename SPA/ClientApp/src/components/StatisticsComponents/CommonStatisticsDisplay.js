d0635dc723cffb68fdd0b2f6693db6b19221767b
Ignacio Tampe <Igtampe@gmail.com>
2022-02-23 16:40:07 -0400

    Add horizontal to graphandlist, also add ElementT  (top)

--- SPA/ClientApp/src/components/StatisticsComponents/CommonStatisticsDisplay.js
+++ SPA/ClientApp/src/components/StatisticsComponents/CommonStatisticsDisplay.js
@@ -4,15 +4,13 @@ import { Card, CardContent, Grid } from "@mui/material";
 import React from "react";
 
 export function GraphAndListStatisticsDisplay(props) {
-
     //MAN I *LOVE* GRIDS!!!!
-
     return (
         <Grid container spacing={2}>
-            <Grid item xs={12}>
+            <Grid item xs={ props.horizontal && !props.Vertical ? 6 : 12}>
                 {props.graph ? props.graph(props) : nada('graph')}
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={props.horizontal && !props.Vertical ? 6 : 12}>
                 {props.list ? props.list(props) : nada('list')}
             </Grid>
         </Grid>
@@ -25,8 +23,15 @@ export default function CommonStatisticsDisplay(props) {
 
     return (
         <div style={{ marginTop: '10px', marginBottom: '10px', ...props.style }}>
-            <h4 style={{ marginBottom: '10px', marginTop: '20px' }}>{props.title ? props.title : 'Statistics Display'}</h4>
+            {props.title ? <><h2 style={{ marginBottom: '10px', marginTop: '20px', textAlign:'center' }}>{props.title}</h2></> : <></>}
             <Grid container spacing={2}>
+                {props.elementT
+                    ? <Grid item xs={12}>
+                        <Card><CardContent>
+                            {props.elementT ? props.elementT(props) : nada('elementT')}
+                        </CardContent></Card>
+                    </Grid>
+                    : <></>}
                 <Grid item xs={props.Vertical ? 12 : 6}>
                     <Card> <CardContent>
                             {props.elementL ? props.elementL(props) : nada('elementL')}
