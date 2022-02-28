e79da940b8278b8cbba3fb0e47a0703a15175899
Ignacio Tampe <Igtampe@gmail.com>
2022-02-28 18:37:55 -0400

    Import Cleanup

--- V2N/Forms/Details/DetailsFormGenerator.cs
+++ V2N/Forms/Details/DetailsFormGenerator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Igtampe.Neco.V2N.Forms.Details {
+﻿namespace Igtampe.Neco.V2N.Forms.Details {
     public static class DetailsFormGenerator {
 
         public static DetailsForm GenerateDetailsForm(ViBE.User VUser) {
