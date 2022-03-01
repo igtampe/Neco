d0a340eac2604470d3982a43c97dddacaa87bbb3
Ignacio Tampe <Igtampe@gmail.com>
2022-02-28 20:59:08 -0400

    Add APIURL to SetupProxy

--- SPA/ClientApp/src/setupProxy.js
+++ SPA/ClientApp/src/setupProxy.js
@@ -1,12 +1,17 @@
 const createProxyMiddleware = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:39327';
+const target = env.APIURL
+  ? env.APIURL
+  : env.ASPNETCORE_HTTPS_PORT
+    ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+    : env.ASPNETCORE_URLS
+      ? env.ASPNETCORE_URLS.split(';')[0]
+      : 'http://localhost:39327';
 
-const context =  [ "/API", "/Swagger"];
+const context = ["/API", "/Swagger"];
 
-module.exports = function(app) {
+module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {
     target: target,
     secure: false
